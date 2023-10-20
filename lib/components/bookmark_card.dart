5e16c882c91552825964b8502f9609bc91e8c484
RafihYahya <rafihyahya@gmail.com>
2023-10-20 14:06:32 +0100

    smoll_commit

---
+++ lib/components/bookmark_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BKCard extends StatelessWidget {
+  const BKCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/bookmark_page.dart
+++ lib/pages/bookmark_page.dart
@@ -14,44 +14,64 @@ class _BookMarkPageState extends State<BookMarkPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: widget.dark!
-          ? const Color.fromARGB(255, 39, 39, 39)
-          : const Color.fromARGB(255, 221, 221, 221),
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Home Screen',
-            style: widget.dark! == true
-                ? const TextStyle(color: Colors.white, fontSize: 18.0)
-                : const TextStyle(color: Colors.black, fontSize: 18.0),
-          ),
-        ),
         backgroundColor: widget.dark!
-            ? const Color.fromARGB(255, 0, 116, 10)
-            : const Color(0xFF04B400),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
-            color: Colors.black,
-            iconSize: 28.0,
+            ? const Color.fromARGB(255, 39, 39, 39)
+            : const Color.fromARGB(255, 221, 221, 221),
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Home Screen',
+              style: widget.dark! == true
+                  ? const TextStyle(color: Colors.white, fontSize: 18.0)
+                  : const TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
           ),
-          IconButton(
-            onPressed: () {
-              widget.callback2();
-            },
-            icon: !widget.dark!
-                ? const Icon(Icons.dark_mode)
-                : const Icon(Icons.light_mode),
-            color: Colors.black,
-            iconSize: 28.0,
+          backgroundColor: widget.dark!
+              ? const Color.fromARGB(255, 0, 116, 10)
+              : const Color(0xFF04B400),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              color: Colors.black,
+              iconSize: 28.0,
+            ),
+            IconButton(
+              onPressed: () {
+                widget.callback2();
+              },
+              icon: !widget.dark!
+                  ? const Icon(Icons.dark_mode)
+                  : const Icon(Icons.light_mode),
+              color: Colors.black,
+              iconSize: 28.0,
+            ),
+          ],
+        ),
+        drawer: Drawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 14.0, horizontal: 4.0),
+                  height: 100,
+                  child: Text('BOOKMARKS', style: TextStyle(fontSize: 36.0))),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: (context, index) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(child: Center(child: Text('Placeholder'))),
+                            Expanded(child: Center(child: Text('Placeholder')))
+                          ],
+                        )),
+              ),
+            ],
           ),
-        ],
-      ),
-      drawer: Drawer(),
-      body: Container(
-        child: null,
-      ),
-    );
+        ));
   }
 }
