6b07a81dd93470836041ba729125af679c6434c9
RafihYahya <rafihyahya@gmail.com>
2023-10-21 15:05:41 +0100

    ..

--- lib/pages/desc_page.dart
+++ lib/pages/desc_page.dart
@@ -7,7 +7,16 @@ class DescPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(),
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D'),
+                  fit: BoxFit.cover)),
+          height: 300,
+          width: MediaQuery.of(context).size.width,
+          child: null,
+        ),
         Text('data'),
         Text('Desc'),
       ],
