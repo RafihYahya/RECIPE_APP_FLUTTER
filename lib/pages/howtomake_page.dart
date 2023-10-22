a0dae0a977d2c7342b913e9300274a43b06ff441
RafihYahya <rafihyahya@gmail.com>
2023-10-22 15:42:56 +0100

    howto_makeui_donev0

--- lib/pages/howtomake_page.dart
+++ lib/pages/howtomake_page.dart
@@ -12,139 +12,209 @@ class HowToPage extends StatelessWidget {
       body: Stack(children: [
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 32.0, horizontal: 12.0),
-                child: Text(
-                  'INGREDIENTS',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF04B400)),
+          child: SingleChildScrollView(
+            physics: ScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 32.0, horizontal: 12.0),
+                  child: Text(
+                    'INGREDIENTS',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF04B400)),
+                  ),
                 ),
-              ),
-              Container(
-                height: 250,
-                width: MediaQuery.of(context).size.width,
-                child: PageView.builder(
-                  physics: PageScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 15,
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    color: !dark!
-                                        ? Color(0xFFC7C7C7)
-                                        : Color.fromARGB(255, 54, 54, 54),
-                                  ),
-                                  height: 100,
-                                  child: Center(
-                                      child: Text(
-                                    '10 EGG',
-                                    style: TextStyle(
-                                        fontSize: 18.0,
-                                        color: dark!
-                                            ? Colors.white
-                                            : Colors.black),
-                                  ))),
-                            ),
-                            SizedBox(
-                              width: 28.0,
-                            ),
-                            Expanded(
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    color: !dark!
-                                        ? Color(0xFFC7C7C7)
-                                        : Color.fromARGB(255, 54, 54, 54),
-                                  ),
-                                  height: 100,
-                                  child: Center(
-                                      child: Text('10 EGG',
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: dark!
-                                                  ? Colors.white
-                                                  : Colors.black)))),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 28.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    color: !dark!
-                                        ? Color(0xFFC7C7C7)
-                                        : Color.fromARGB(255, 54, 54, 54),
-                                  ),
-                                  height: 100,
-                                  child: Center(
-                                      child: Text('10 EGG',
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: dark!
-                                                  ? Colors.white
-                                                  : Colors.black)))),
-                            ),
-                            SizedBox(
-                              width: 28.0,
-                            ),
-                            Expanded(
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    color: !dark!
-                                        ? Color(0xFFC7C7C7)
-                                        : Color.fromARGB(255, 54, 54, 54),
-                                  ),
-                                  height: 100,
-                                  child: Center(
-                                      child: Text('10 EGG',
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: dark!
-                                                  ? Colors.white
-                                                  : Colors.black)))),
-                            ),
-                          ],
-                        ),
-                      ],
+                Container(
+                  height: 250,
+                  width: MediaQuery.of(context).size.width,
+                  child: PageView.builder(
+                    physics: PageScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 15,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: !dark!
+                                          ? Color(0xFFC7C7C7)
+                                          : Color.fromARGB(255, 54, 54, 54),
+                                    ),
+                                    height: 100,
+                                    child: Center(
+                                        child: Text(
+                                      '10 EGG',
+                                      style: TextStyle(
+                                          fontSize: 18.0,
+                                          color: dark!
+                                              ? Colors.white
+                                              : Colors.black),
+                                    ))),
+                              ),
+                              SizedBox(
+                                width: 28.0,
+                              ),
+                              Expanded(
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: !dark!
+                                          ? Color(0xFFC7C7C7)
+                                          : Color.fromARGB(255, 54, 54, 54),
+                                    ),
+                                    height: 100,
+                                    child: Center(
+                                        child: Text('10 EGG',
+                                            style: TextStyle(
+                                                fontSize: 18.0,
+                                                color: dark!
+                                                    ? Colors.white
+                                                    : Colors.black)))),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 28.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: !dark!
+                                          ? Color(0xFFC7C7C7)
+                                          : Color.fromARGB(255, 54, 54, 54),
+                                    ),
+                                    height: 100,
+                                    child: Center(
+                                        child: Text('10 EGG',
+                                            style: TextStyle(
+                                                fontSize: 18.0,
+                                                color: dark!
+                                                    ? Colors.white
+                                                    : Colors.black)))),
+                              ),
+                              SizedBox(
+                                width: 28.0,
+                              ),
+                              Expanded(
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: !dark!
+                                          ? Color(0xFFC7C7C7)
+                                          : Color.fromARGB(255, 54, 54, 54),
+                                    ),
+                                    height: 100,
+                                    child: Center(
+                                        child: Text('10 EGG',
+                                            style: TextStyle(
+                                                fontSize: 18.0,
+                                                color: dark!
+                                                    ? Colors.white
+                                                    : Colors.black)))),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(12.0, 36.0, 12.0, 54),
-                child: Text(
-                  'INSTRUCTIONS',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF04B400)),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(12.0, 45.0, 12.0, 36),
+                  child: Text(
+                    'INSTRUCTIONS',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF04B400)),
+                  ),
                 ),
-              ),
-              Text('MIAW')
-            ],
+                ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 20,
+                    itemBuilder: ((context, index) => Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 18.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: index % 2 == 0
+                                    ? const EdgeInsets.only(right: 75.0)
+                                    : EdgeInsets.only(left: 80.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    index % 2 == 0
+                                        ? Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 8.0),
+                                            child: Text(index.toString(),
+                                                style: TextStyle(
+                                                    fontSize: 24,
+                                                    fontWeight: FontWeight.w900,
+                                                    color: Color.fromRGBO(
+                                                        0,
+                                                        0 + index * 20,
+                                                        index,
+                                                        1))),
+                                          )
+                                        : SizedBox(),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8.0),
+                                          color: Color(0xFFB5B5B5)),
+                                      height: 130,
+                                      width: 215,
+                                      child: Center(
+                                          child: Text(
+                                        '',
+                                      )),
+                                    ),
+                                    index % 2 == 1
+                                        ? Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 12.0),
+                                            child: Text(
+                                              index.toString(),
+                                              style: TextStyle(
+                                                  fontSize: 24,
+                                                  fontWeight: FontWeight.w900,
+                                                  color: Color.fromRGBO(
+                                                      0,
+                                                      0 + index * 20,
+                                                      index,
+                                                      1)),
+                                            ),
+                                          )
+                                        : SizedBox(),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )))
+              ],
+            ),
           ),
         ),
         Container(
